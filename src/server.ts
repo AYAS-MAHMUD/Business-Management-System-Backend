ebde40370ab68b1a5dd95b70fc2d378193181a0b
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2026-05-12 14:16:07 +0600

    Server is running

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import http from "http";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.DB_URL as string);
+    console.log("✅ MongoDB Connected");
+
+    server.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log("❌ Database connection failed", error);
+  }
+}
+
+main();
